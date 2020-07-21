db9f3d65520eacf81d633bf4e738610b84ee717d
Jewel Posniak <jewelposniak@gmail.com>
2020-07-20 20:53:06 -0400

    learning more about timing my code

--- main.cpp
+++ main.cpp
@@ -1,90 +1,154 @@
-#include <iostream>
+#include <cmath>
+#include <cstdio>
 #include <vector>
-#include <string>
-
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include <ctime>
 using namespace std;
 
- int findMin(vector<int>& prices)
+/*
+    std::clock_t start;
+    double duration;
+
+    start = std::clock();
+
+
+    duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
+
+    std::cout<<"printf: "<< duration <<'\n';
+*/
+
+
+int main() {
+
+    vector<double> operation1;
+    vector<double> operation2;
+    vector<double> operation3;
+    
+    int loop;
+    cin >> loop;
+    
+    int operation;
+    int number = 0;
+    
+    stack<int> s1;
+    stack<int> s2;
+
+
+    for(int i = 0; i < loop; i++)
     {
-        int min = 0;
+        cin >> operation;
         
-        for(int i = 1; i < prices.size(); i++)
+        if(operation == 1)
         {
-            if(prices[i] < prices[min])
-            {
-                min = i;
-            }
+
+            std::clock_t start;
+            double duration;
+
+            start = std::clock();
+
+            cin >> number;
+            s1.push(number);
+
+            duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
+
+            operation1.push_back(duration);
+
         }
-        
-        return min;
-    }
-    
-    int findMin(vector<int>& prices, int x)
-    {
-        int min = 0;
-        
-        for(int i = min; i < x; i++)
+        if(operation == 2)
         {
-            if(prices[i] < prices[min])
+            
+            std::clock_t start;
+            double duration;
+
+            start = std::clock();
+
+            //flip onto new stack
+            //pop
+            //transfer back onto stack one
+
+            while(!s1.empty())
             {
-                min = i;
+                int temp = s1.top();
+                s2.push(temp);
+                s1.pop();
             }
-        }
-        
-        return min;
-    }
-    
-    int findMax(vector<int>& prices)
-    {
-        int max = 0;
-        
-        for(int i = 1; i < prices.size(); i++)
+            
+            s2.pop();
+            
+            while(!s2.empty())
+            {
+                int temp = s2.top();
+                s1.push(temp);
+                s2.pop();
+            }
+
+            duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
+
+            operation2.push_back(duration);
+        }   
+        if(operation == 3)
         {
-            if(prices[i] >= prices[max])
+            std::clock_t start;
+            double duration;
+
+            start = std::clock();
+
+            //flip onto new stack
+            //get the top element
+            
+            while(!s1.empty())
             {
-                max = i;
+                int temp = s1.top();
+                s2.push(temp);
+                s1.pop();
             }
-        }
-        
-        return max;
-    }
-    
-    int findMax(vector<int>& prices, int x)
-    {
-        int max = x;
-        
-        for(int i = x; i < prices.size(); i++)
-        {
-            if(prices[i] >= prices[max])
+            
+            cout << s2.top() << endl;
+            
+            while(!s2.empty())
             {
-                max = i;
+                int temp = s2.top();
+                s1.push(temp);
+                s2.pop();
             }
+
+            duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
+
+            operation3.push_back(duration);
+            
         }
-        
-        return max;
-    }
-    
-    int CompareTo(int x, int y)
-    {
-        if(x > y)
-            return x;
-        else return y;
-    }
-    
-    
-    int maxProfit(vector<int>& prices) {
-        int min = findMin(prices);
-        int max = findMax(prices);
-        int maxCap = findMax(prices, min);
-        int minCap = findMin(prices, max);
-        
-        return CompareTo(prices[maxCap]-prices[min], prices[max]-prices[minCap]);
     }
 
-    int main(int argc, char const *argv[])
+    double total = 0;
+
+    for(double d : operation1)
     {
-        vector<int> v;
-        cout << maxProfit(v) << endl;
-        return 0;
+        total += d;
     }
+
+    double avg1 = total / operation1.size();
+    total = 0;
+
+    for(double d : operation2)
+    {
+        total += d;
+    }
+
+    double avg2 = total / operation2.size();
+    total = 0;
     
+    for(double d : operation3)
+    {
+        total += d;
+    }
+
+    double avg3 = total / operation3.size();
+
+    cout << "average 1: " << avg1 << endl;
+    cout << "average 2: " << avg2 << endl;
+    cout << "average 3: " << avg3 << endl;
+
+    return 0;
+}
